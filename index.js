a91e8538179aa46d8f81c6d7fae0dc55a2b64d1b
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-15 14:53:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+let activity;
+function saturdayFun(activity ) {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`}
+@@ -8,9 +9,10 @@ function saturdayFun(activity ) {
+
+}
+//
+function mondayWork(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+let mondayActivity;
+function mondayWork(mondayActivity) {
+    if (mondayActivity) {
+        return `This Monday, I will work from home.`
+    }
+    else{
+        return 'This Monday, I will go to the office.'
+    }
+}
+//
+function wrapAdjective(symbol='*'){
+    return function(word = "special") {
+        return `You are ${symbol}${word}${symbol}!`
+
+    }
